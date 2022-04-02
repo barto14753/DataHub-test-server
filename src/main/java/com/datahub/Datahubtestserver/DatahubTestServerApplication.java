d79defc177af8fb53ba18f0dda9f46a35fde4771
barto14753 <wlodarski_bartosz@o2.pl>
2022-04-02 15:40:44 +0200

    init spring

---
+++ src/main/java/com/datahub/Datahubtestserver/DatahubTestServerApplication.java
@@ -0,0 +1,13 @@
+package com.datahub.Datahubtestserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatahubTestServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatahubTestServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/datahub/Datahubtestserver/DatahubTestServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datahub.Datahubtestserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatahubTestServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
