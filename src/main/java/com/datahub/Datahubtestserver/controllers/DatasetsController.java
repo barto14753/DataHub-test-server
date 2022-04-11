66bd5ca0c1226aa17089c03a5edab85a8ff72c38
barto14753 <wlodarski_bartosz@o2.pl>
2022-04-11 15:03:20 +0200

    add @CrossOrigin

--- src/main/java/com/datahub/Datahubtestserver/controllers/DatasetsController.java
+++ src/main/java/com/datahub/Datahubtestserver/controllers/DatasetsController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/datasets")
 public class DatasetsController {
     private Datasets datasets = Datasets.getInstance();
