e486c4df101f3e41551e542ca3b83bc7e2afd51e
wasacz <wasik.michal22@gmail.com>
2022-05-16 17:38:59 +0200

    Timestamp + Downloader update

--- src/main/java/com/datahub/Datahubtestserver/model/Timestamp.java
+++ src/main/java/com/datahub/Datahubtestserver/model/Timestamp.java
@@ -1,41 +1,59 @@
 package com.datahub.Datahubtestserver.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import javax.validation.constraints.Null;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Timestamp {
     private String from;// non datetime format options: always
-    private final String to; // non datetime format options: now
+    private String to; // non datetime format options: now
     private static final SimpleDateFormat dateTimeFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX");
 
     private static final long MILLIS_IN_A_HOUR = 1000 * 60 * 60;
     private static final long MILLIS_IN_A_DAY = 1000 * 60 * 60 * 24;
     private static final long MILLIS_IN_A_WEEK = 1000 * 60 * 60 * 24 * 7;
+
+    public Long FROM_MILLIS = null;
+    public Long TO_MILLIS = null;
+    public boolean fromNow = false;
     public Timestamp(
             @JsonProperty("from") String from,
             @JsonProperty("to") String to)
     {
         try {
             this.from = getFrom(from,to);
+            if(!from.equals("always")){
+                this.FROM_MILLIS = Timestamp.dateTimeFormat.parse(this.from).getTime();
+            }
+            this.to = to;
+            if(!to.equals("now")){
+                this.TO_MILLIS = Timestamp.dateTimeFormat.parse(this.to).getTime();
+            }
+            else{
+                this.fromNow = true;
+                this.TO_MILLIS = new Date().getTime();
+            }
         } catch (ParseException e) {
             e.printStackTrace();
         }
-        this.to = to;
     }
 
     private String getFrom(String from,String to) throws ParseException {
-        Date toDate = new Date();
+        long millis= new Date().getTime();
         if(!to.equals("now")){
-            toDate = Timestamp.dateTimeFormat.parse(to);
+            millis = Timestamp.dateTimeFormat.parse(to).getTime();
+            System.out.println(Timestamp.dateTimeFormat.parse(to).toString());
         }
         return switch (from) {
-            case "hour" -> Timestamp.dateTimeFormat.format(toDate.getTime() - MILLIS_IN_A_HOUR);
-            case "day" -> Timestamp.dateTimeFormat.format(toDate.getTime() - MILLIS_IN_A_DAY);
-            case "week" -> Timestamp.dateTimeFormat.format(toDate.getTime() - MILLIS_IN_A_WEEK);
-            case "month" -> Timestamp.dateTimeFormat.format(toDate.getTime() - 4 * MILLIS_IN_A_WEEK);
+            case "hour" -> Timestamp.dateTimeFormat.format(millis - MILLIS_IN_A_HOUR);
+            case "day" -> Timestamp.dateTimeFormat.format(millis - MILLIS_IN_A_DAY);
+            case "week" -> Timestamp.dateTimeFormat.format(millis - MILLIS_IN_A_WEEK);
+            case "month" -> Timestamp.dateTimeFormat.format(millis - 4 * MILLIS_IN_A_WEEK);
             default -> from;
         };
     }
@@ -57,27 +75,15 @@ public class Timestamp {
     }
 
     public boolean isInRange(String datetime) throws ParseException {
-        Date date = getDate(datetime);
-        if (from.equals("always"))
-        {
-            if (to.equals("now")) return true;
-            else
-            {
-                Date _to = getDate(to);
-                return _to.after(date);
-            }
+        long millis = Timestamp.dateTimeFormat.parse(datetime).getTime();
+        boolean result = false;
+        if(!(FROM_MILLIS == null) && millis <= TO_MILLIS && millis >= FROM_MILLIS){
+            result = true;
         }
-        else
-        {
-            Date from_ = getDate(from);
-            if (to.equals("now")) return from_.before(date);
-            else
-            {
-                Date _to = getDate(to);
-                return _to.after(date);
-            }
+        else if(millis <= TO_MILLIS){
+            result = true;
         }
-
+        return result;
     }
 
     public boolean isLate(String datetime) throws ParseException {
@@ -89,5 +95,16 @@ public class Timestamp {
             return _from.after(date);
         }
 
+    }
+
+    public static long oneResultDifference(JSONObject jsonObject, JSONObject jsonObject1) throws JSONException, ParseException {
+        String datetime1 = jsonObject.getString("timestamp");
+        String datetime2 = jsonObject1.getString("timestamp");
+
+        return Timestamp.dateTimeFormat.parse(datetime1).getTime() - Timestamp.dateTimeFormat.parse(datetime2).getTime();
+    }
+
+    public static long getMillis(JSONObject jsonObject) throws JSONException, ParseException {
+        return Timestamp.dateTimeFormat.parse(jsonObject.getString("timestamp")).getTime();
     }
 }
