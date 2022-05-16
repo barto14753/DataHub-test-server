20a92e7f6529fba8f5c935e386d58eeb15639d22
wasacz <wasik.michal22@gmail.com>
2022-05-16 19:02:01 +0200

    download pdf?

--- src/main/java/com/datahub/Datahubtestserver/controllers/DatasetsController.java
+++ src/main/java/com/datahub/Datahubtestserver/controllers/DatasetsController.java
@@ -4,8 +4,12 @@ import com.datahub.Datahubtestserver.model.Data;
 import com.datahub.Datahubtestserver.model.Dataset;
 import com.datahub.Datahubtestserver.model.Record;
 import com.datahub.Datahubtestserver.state.Datasets;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.InputStream;
 import java.util.List;
 
 @RestController
@@ -16,37 +20,39 @@ public class DatasetsController {
 
     @GetMapping("/{dataset}")
     @ResponseBody
-    public Dataset getDataset(@PathVariable String dataset)
-    {
+    public Dataset getDataset(@PathVariable String dataset) {
         return datasets.getDataset(dataset);
     }
 
     @GetMapping("/{dataset}/{data}")
     @ResponseBody
-    public Data getDataset(@PathVariable String dataset, @PathVariable String data)
-    {
+    public Data getDataset(@PathVariable String dataset, @PathVariable String data) {
         return datasets.getDataset(dataset).getData(data);
     }
 
     @GetMapping("/{dataset}/{data}/records")
     @ResponseBody
-    public List<Record> getData(@PathVariable String dataset, @PathVariable String data)
-    {
+    public List<Record> getData(@PathVariable String dataset, @PathVariable String data) {
         return datasets.getDataset(dataset).getData(data).getRecords();
     }
 
     @GetMapping("/all")
     @ResponseBody
-    public List<Dataset> getAllDatasets()
-    {
+    public List<Dataset> getAllDatasets() {
         return datasets.getDatasets();
     }
 
     @GetMapping("/allNames")
     @ResponseBody
-    public List<String> getAllDatasetsNames()
-    {
+    public List<String> getAllDatasetsNames() {
         return datasets.getDatasetsNames();
+    }
+
+
+    @RequestMapping(value = "/documentation", method = RequestMethod.GET)
+    @ResponseBody
+    public FileSystemResource getFile() {
+        return new FileSystemResource("src/main/java/com/datahub/Datahubtestserver/config/documentation.pdf");
     }
 
 
